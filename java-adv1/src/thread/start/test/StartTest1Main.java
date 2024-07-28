95132927e172e4f2e03095cd4731caf97f58b817
q1q1w2w2 <106463879+q1q1w2w2@users.noreply.github.com>
2024-07-28 19:52:53 +0900

    2024.07.28 자바 고급1편 시작
    스레드

---
+++ java-adv1/src/thread/start/test/StartTest1Main.java
@@ -0,0 +1,26 @@
+package thread.start.test;
+
+import static util.MyLogger.log;
+
+public class StartTest1Main {
+    public static void main(String[] args) {
+        log("main() start");
+        CounterThread counterThread = new CounterThread();
+        counterThread.start();
+        log("main() end");
+    }
+
+    static class CounterThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 1; i <= 5; i++) {
+                log("value: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
